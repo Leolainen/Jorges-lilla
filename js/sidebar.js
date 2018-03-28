2ea8ab0f0c78f9cc370e02d36f866d02ccd2529f
leolainen <leolainen2@gmail.com>
2018-03-28 19:20:31 +0200

    refined search function

--- js/sidebar.js
+++ js/sidebar.js
@@ -16,30 +16,32 @@ const sidebar = (function() {
     for (var i = 0; i < races.length; i++) {
       let currentMob = sortByRace(monsterData, races[i]);
 
-      let newLabel = document.createElement("h3");
-      newLabel.setAttribute("class", "side-bar_label");
-      let label = document.createTextNode(races[i]);
-      newLabel.appendChild(label);
-      raceList.appendChild(newLabel);
+      if (currentMob.length > 0) {
+        let newLabel = document.createElement("h3");
+        newLabel.setAttribute("class", "side-bar_label");
+        let label = document.createTextNode(races[i]);
+        newLabel.appendChild(label);
+        raceList.appendChild(newLabel);
 
-      currentMob.forEach(function(mob) {
-        let newLi = document.createElement("LI");
-        let liText = document.createTextNode(mob.art);
-        newLi.appendChild(liText);
-        newLi.onclick = function() {
-          pageBuilder.buildHTML(mob.id);
-          sideBarBtn.classList.toggle("side-bar_button-active");
-          sideBar.classList.toggle("side-bar_active");
-          searchBar.value = "";
-          setTimeout(function() {
-            buildList();
-          }, 200);
-        }
-        raceList.appendChild(newLi);
-      });
+        currentMob.forEach(function(mob) {
+          let newLi = document.createElement("LI");
+          let liText = document.createTextNode(mob.art);
+          newLi.appendChild(liText);
+          newLi.onclick = function() {
+            pageBuilder.buildHTML(mob.id);
+            sideBarBtn.classList.toggle("side-bar_button-active");
+            sideBar.classList.toggle("side-bar_active");
+            searchBar.value = "";
+            setTimeout(function() {
+              buildList();
+            }, 200);
+          }
+          raceList.appendChild(newLi);
+        });
 
-      let lineBreak = document.createElement("br");
-      raceList.appendChild(lineBreak);
+        let lineBreak = document.createElement("br");
+        raceList.appendChild(lineBreak);
+      }
     }
   }
 
