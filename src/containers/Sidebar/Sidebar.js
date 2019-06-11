43c944bbb81759f372482e919dd0095e5f30b030
leolainen <leolainen2@gmail.com>
2019-06-11 17:48:29 +0200

    [Sidebar] Sidebar now shows al species

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { makeStyles } from '@material-ui/styles';
+
+import { useAppContext } from '../AppContext';
+
 import Drawer from '../../components/Drawer';
+import List from '../../components/List';
+import ListItem from '../../components/ListItem';
+import ListItemText from '../../components/ListItemText';
+import ListSubHeader from '../../components/ListSubHeader';
+
+import { getAllEntries } from '../../api/bestiary';
 
 const useStyles = makeStyles((theme) => ({
-	root: {}
+	root: {},
+	list: {
+		minWidth: 250
+	},
+	divider: {
+		opacity: 0.6,
+		marginTop: theme.spacing(4)
+	}
 }));
 
 const Sidebar = React.forwardRef(function Sidebar(props, ref) {
 	const { className, children, ...other } = props;
+
+	const [ { entries: bestiary }, dispatch ] = useAppContext();
+
 	const classes = useStyles();
+	const [ ancestry, setAncestry ] = useState([]);
+
+	useEffect(
+		() => {
+			setAncestry([ ...new Set(bestiary.map((beast) => beast.ancestry)) ]);
+			/* setAncestry([ ...new Set(bestiary.map((beast) => beast.ancestry)) ]); */
+		},
+		[ bestiary ]
+	);
+
+	console.log('ancestry', ancestry);
 
 	return (
 		<Drawer anchor="right" ref={ref} className={classnames(classes.root, className)} {...other}>
-			{children}
+			<div role="presentation" className={classes.list}>
+				<List>
+					{ancestry.map((ancestor) => (
+						<React.Fragment key={ancestor}>
+							<ListSubHeader disableSticky>{ancestor}</ListSubHeader>
+							{bestiary.map(
+								(beast) =>
+									beast.ancestry === ancestor && (
+										<ListItem button key={beast.species}>
+											<ListItemText primary={beast.species} />
+										</ListItem>
+									)
+							)}
+							<hr className={classes.divider} />
+						</React.Fragment>
+					))}
+				</List>
+			</div>
 		</Drawer>
 	);
 });
@@ -30,3 +77,13 @@ Sidebar.defaultProps = {
 Sidebar.uiName = 'Sidebar';
 
 export default Sidebar;
+/* {ancestry.map(ancestor => {
+	b
+	return (
+
+	<ListItem>
+
+		<ListItemText primary={beast.species}
+	</ListItem>
+	)
+})} */
