e9e126d0039d861f36300c249f27126dbdce4403
leolainen <leolainen2@gmail.com>
2019-06-12 22:53:34 +0200

    [Page] Updated layout. - Made layout more responsive. Added css Grid

--- src/containers/Page/page.js
+++ src/containers/Page/page.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+
 import { makeStyles } from '@material-ui/styles';
+import { useTheme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { useAppContext } from '../AppContext';
 import { TOGGLE_SIDEBAR } from '../AppContext/constants';
 import Sidebar from '../Sidebar';
 
+import Box from '../../components/Box';
 import IconButton from '../../components/IconButton';
 import Container from '../../components/Container';
 import AppBar from '../../components/AppBar';
 import Toolbar from '../../components/Toolbar';
+import Hidden from '../../components/Hidden';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		backgroundColor: '#78909c',
-		minWidth: '100vw',
-		minHeight: '100vh'
+		maxWidth: '100vw',
+		height: '100vh',
+		display: 'grid',
+		[theme.breakpoints.up('md')]: {
+			grid: 'auto-flow dense / 1fr 250px'
+		}
 	},
 	hamburger: {
 		marginLeft: 'auto'
@@ -31,6 +39,8 @@ const useStyles = makeStyles((theme) => ({
 const Page = React.forwardRef(function Page(props, ref) {
 	const { className, children, ...other } = props;
 	const [ { sidebarIsOpen }, dispatch ] = useAppContext();
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
 	const handleMenuClick = () => {
 		dispatch({
@@ -41,15 +51,19 @@ const Page = React.forwardRef(function Page(props, ref) {
 
 	return (
 		<main ref={ref} className={classnames(classes.root, className)} {...other}>
-			<AppBar className={classes.header}>
-				<Toolbar variant="dense">
-					<IconButton aria-label="menu" className={classes.hamburger} onClick={handleMenuClick}>
-						<MenuIcon />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-			<Sidebar open={sidebarIsOpen} onClose={handleMenuClick} />
-			<Container maxWidth="lg">{children}</Container>
+			<Hidden mdUp>
+				<AppBar className={classes.header}>
+					<Toolbar variant="dense">
+						<IconButton aria-label="menu" className={classes.hamburger} onClick={handleMenuClick}>
+							<MenuIcon />
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+			</Hidden>
+			<Container maxWidth="md">
+				<Box mt={16}>{children}</Box>
+			</Container>
+			<Sidebar open={sidebarIsOpen} onClose={handleMenuClick} variant={isMobile ? 'temporary' : 'permanent'} />
 		</main>
 	);
 });
