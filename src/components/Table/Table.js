48e2377eeb1d7385c4673df681766ae40d51b3aa
leolainen <leolainen2@gmail.com>
2019-06-15 23:21:25 +0200

    [Table] Updated Table and subcomponents

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,1 +1,1 @@
-export { Table } from '@material-ui/core';
+export { default } from '@material-ui/core/Table';
--- src/components/TableBody/TableBody.js
+++ src/components/TableBody/TableBody.js
@@ -1,1 +1,1 @@
-export { TableBody } from '@material-ui/core';
+export { default } from '@material-ui/core/TableBody';
