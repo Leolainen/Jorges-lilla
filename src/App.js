ddea392f3f0d33413e8e5ae5ca7b10083f03ea44
leolainen <leolainen2@gmail.com>
2019-06-15 23:24:50 +0200

    [App] Use blocks instead of inline coding. Started on grid layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/styles';
 
 import { useAppContext } from './containers/AppContext';
 import { SET_CURRENT_ENTRY, SET_ENTRIES } from './containers/AppContext/constants';
 import Page from './containers/Page';
+
+import AttributesBlock from './blocks/AttributesBlock';
 
 import bestiaryData from './data/bestiary';
 
 import Card from './components/Card';
 import CardMedia from './components/CardMedia';
 import CardContent from './components/CardContent';
+import Typography from './components/Typography';
+import Table from './components/Table';
+import TableHead from './components/TableHead';
+import TableBody from './components/TableBody';
+import TableCell from './components/TableCell';
+import TableRow from './components/TableRow';
+import Loader from './components/Loader';
+
+import { isEmpty } from './utils/helpers';
+
+const useStyles = makeStyles((theme) => ({
+	root: {},
+	wrapper: {
+		display: 'grid',
+		gridTemplateColumns: '1fr 1fr',
+		gridTemplateRows: 'auto'
+	}
+	/* .grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas: "card desc" "card desc" "second desc" "second desc" "body desc" "body ." ". .";
+  }
+  
+  .card { grid-area: card; }
+  
+  .desc { grid-area: desc; }
+  
+  .second { grid-area: second; }
+  
+  .body { grid-area: body; } */
+}));
 
 const App = (props) => {
+	const { className, children, ...other } = props;
 	const [ { currentEntry }, dispatch ] = useAppContext();
+	const classes = useStyles();
 
 	/* Initialize bestiary */
 	useEffect(() => {
@@ -40,22 +77,29 @@ const App = (props) => {
 
 	const images = importAll(require.context('./assets/images/', false, /\.(png|PNG)$/));
 
-	/* const dynamicImportImage = (imgName) => require.context(`../assets/images/${imgName}.PNG`, false, /\.(png|PNG)$/); */
-
-	// const image = images.find((img) => img === `${currentEntry.img_name}.PNG`);
-	console.log('images', images, images[`${currentEntry.img_name}.PNG`]);
-
 	return (
 		<Page>
-			<Card>
-				<CardMedia
-					component="img"
-					alt={currentEntry.species}
-					height="140"
-					image={images[`${currentEntry.img_name}.PNG`]}
-					title={currentEntry.species}
-				/>
-			</Card>
+			{isEmpty(currentEntry) ? (
+				<Loader />
+			) : (
+				<main className={classes.wrapper}>
+					<Card>
+						<CardMedia
+							component="img"
+							alt={currentEntry.species}
+							height="500"
+							image={images[`${currentEntry.img_name}.PNG`]}
+							title={currentEntry.species}
+						/>
+						<CardContent>
+							<Typography gutterBottom variant="h5" component="h2">
+								{currentEntry.species}
+							</Typography>
+							<AttributesBlock attributes={currentEntry.attributes} />
+						</CardContent>
+					</Card>
+				</main>
+			)}
 		</Page>
 	);
 };
