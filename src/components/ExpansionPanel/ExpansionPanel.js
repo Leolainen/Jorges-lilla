44c5efc111d1b3549ed4926a72867f5cfe959c84
leolainen <leolainen2@gmail.com>
2019-07-06 13:51:36 +0200

    [ExpansionPanel] Remove margin from expanded panel

--- src/components/ExpansionPanel/ExpansionPanel.js
+++ src/components/ExpansionPanel/ExpansionPanel.js
@@ -2,6 +2,9 @@ export { default } from '@material-ui/core/ExpansionPanel';
 
 export const styles = (theme) => ({
 	root: {
+		'&$expanded': {
+			margin: 0
+		},
 		'&:before': {
 			display: 'none'
 		}
