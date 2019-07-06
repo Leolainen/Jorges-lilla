a0febf3b4df4b49fb9f1009815669dfcd69105c5
leolainen <leolainen2@gmail.com>
2019-07-06 13:54:35 +0200

    [App] Added rest of content

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
 import React, { useEffect } from 'react';
-import { makeStyles } from '@material-ui/styles';
 
 import { useAppContext } from './containers/AppContext';
 import { SET_CURRENT_ENTRY, SET_ENTRIES } from './containers/AppContext/constants';
 import Page from './containers/Page';
 
 import AttributesBlock from './blocks/AttributesBlock';
+import BodypartsBlock from './blocks/BodypartsBlock';
+import CombatBlock from './blocks/CombatBlock';
+import AbilitiesBlock from './blocks/AbilitiesBlock';
 
 import bestiaryData from './data/bestiary';
 
+import Box from './components/Box';
 import Card from './components/Card';
 import CardMedia from './components/CardMedia';
 import CardContent from './components/CardContent';
 import Typography from './components/Typography';
-import Table from './components/Table';
-import TableHead from './components/TableHead';
-import TableBody from './components/TableBody';
-import TableCell from './components/TableCell';
-import TableRow from './components/TableRow';
+import Grid from './components/Grid';
+import VerticalRhythm from './components/VerticalRhythm';
+
 import Loader from './components/Loader';
 
 import { isEmpty } from './utils/helpers';
 
-const useStyles = makeStyles((theme) => ({
-	root: {},
-	wrapper: {
-		display: 'grid',
-		gridTemplateColumns: '1fr',
-		gridTemplateRows: 'auto',
-
-		[theme.breakpoints.up('md')]: {
-			gridTemplateColumns: '1fr 1fr'
-		}
-	}
-	/* .grid-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-areas: "card desc" "card desc" "second desc" "second desc" "body desc" "body ." ". .";
-  }
-  
-  .card { grid-area: card; }
-  
-  .desc { grid-area: desc; }
-  
-  .second { grid-area: second; }
-  
-  .body { grid-area: body; } */
-}));
-
 const App = (props) => {
-	const { className, children, ...other } = props;
 	const [ { currentEntry }, dispatch ] = useAppContext();
-	const classes = useStyles();
 
 	/* Initialize bestiary */
 	useEffect(() => {
@@ -86,23 +58,104 @@ const App = (props) => {
 			{isEmpty(currentEntry) ? (
 				<Loader />
 			) : (
-				<main className={classes.wrapper}>
-					<Card>
-						<CardMedia
-							component="img"
-							alt={currentEntry.species}
-							height="500"
-							image={images[`${currentEntry.img_name}.PNG`]}
-							title={currentEntry.species}
-						/>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="h2">
-								{currentEntry.species}
-							</Typography>
-							<AttributesBlock attributes={currentEntry.attributes} />
-						</CardContent>
-					</Card>
-				</main>
+				<Grid component="main" container spacing={3}>
+					<Grid item xs={12} md={6}>
+						<Card>
+							<CardMedia
+								component="img"
+								alt={currentEntry.species}
+								height="500"
+								image={images[`${currentEntry.img_name}.PNG`]}
+								title={currentEntry.species}
+							/>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="h2">
+									{currentEntry.species}
+								</Typography>
+								<Box mt={4} />
+								<Typography>{currentEntry.description}</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+
+					<Grid item xs={12} md={6}>
+						<VerticalRhythm spacing={4}>
+							<Card>
+								<CardContent>
+									<Typography gutterBottom variant="h6" component="h3">
+										Grundegenskaper
+									</Typography>
+									<AttributesBlock attributes={currentEntry.attributes} />
+								</CardContent>
+							</Card>
+
+							<Card>
+								<CardContent>
+									<Typography gutterBottom variant="h6" component="h3">
+										Grundläggande information
+									</Typography>
+									<AttributesBlock attributes={currentEntry.basic_info} />
+								</CardContent>
+							</Card>
+						</VerticalRhythm>
+					</Grid>
+
+					<Grid item xs={12} md={6}>
+						<Card>
+							<CardContent>
+								<Grid item container xs={12}>
+									<Grid item xs={12}>
+										<Typography gutterBottom variant="h6" component="h3">
+											Kroppspoäng
+										</Typography>
+									</Grid>
+									<Grid item xs={12}>
+										<Typography>
+											TKP-sys: {currentEntry.hitpoints.total_hitpoints_system}
+										</Typography>
+										<Typography>TKP: {currentEntry.hitpoints.tkp}</Typography>
+									</Grid>
+									<Grid item xs={12}>
+										<BodypartsBlock bodyparts={currentEntry.bodypart} />
+									</Grid>
+								</Grid>
+							</CardContent>
+						</Card>
+					</Grid>
+
+					<Grid item xs={12} md={6}>
+						<Card>
+							<CardContent>
+								<Typography gutterBottom variant="h6" component="h3">
+									Strid
+								</Typography>
+								<CombatBlock attacks={currentEntry.combat} />
+							</CardContent>
+						</Card>
+					</Grid>
+
+					<Grid item xs={12} md={6}>
+						<Card>
+							<CardContent>
+								<Typography gutterBottom variant="h6" component="h3">
+									Färdigheter
+								</Typography>
+								<CombatBlock attacks={currentEntry.skills} />
+							</CardContent>
+						</Card>
+					</Grid>
+
+					<Grid item xs={12} md={6}>
+						<Card>
+							<CardContent>
+								<Typography gutterBottom variant="h6" component="h3">
+									Förmågor
+								</Typography>
+								<AbilitiesBlock abilities={currentEntry.abilities} />
+							</CardContent>
+						</Card>
+					</Grid>
+				</Grid>
 			)}
 		</Page>
 	);
