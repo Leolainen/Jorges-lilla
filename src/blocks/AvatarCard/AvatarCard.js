67ad635c4ab91fe5150ab956891f78ef4035936d
leolainen <leolainen2@gmail.com>
2019-08-11 16:45:11 +0200

    [AvatarCard] Fix bug where some descriptions disappear on certain viewports

--- src/blocks/AvatarCard/AvatarCard.js
+++ src/blocks/AvatarCard/AvatarCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { makeStyles } from '@material-ui/styles';
 
 import Card from '../../components/Card';
@@ -8,7 +9,10 @@ import CardContent from '../../components/CardContent';
 import Typography from '../../components/Typography';
 
 const useStyles = makeStyles((theme) => ({
-	root: {},
+	root: {
+		display: 'flex',
+		flexDirection: 'column'
+	},
 	avatar: {
 		height: '100%',
 		objectFit: 'contain'
@@ -39,7 +43,7 @@ const AvatarCard = (props) => {
 	const images = importAll(require.context('../../assets/images/', false, /\.(png|PNG)$/));
 
 	return (
-		<Card className={className} {...other}>
+		<Card className={classnames(classes.root, className)} {...other}>
 			<CardMedia
 				component="img"
 				alt={entry.species}
