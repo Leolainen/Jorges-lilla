27da24d8a0f1d5ca73706e30a5d82be7218beb64
leolainen <leolainen2@gmail.com>
2019-09-12 21:05:34 +0200

    [Context] Add currentTab to constants

--- src/containers/AppContext/constants.js
+++ src/containers/AppContext/constants.js
@@ -1,7 +1,8 @@
 export const INITIAL_STATE = {
 	sidebarIsOpen: false,
 	currentEntry: {},
-	entries: []
+	entries: [],
+	currentTab: 'bestiary'
 };
 
 export const TOGGLE_SIDEBAR = 'toggleSidebar';
